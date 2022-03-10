56a563a53e60b42662b3df9199af72602e757015
divvya007 <divvyatripaathi@gmail.com>
2022-03-10 21:14:22 +0000

    added tests for SymbolO

---
+++ src/DisplaySymbols/SymbolO/index.test.js
@@ -0,0 +1,16 @@
+import { expect } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+
+import SymbolO from ".";
+
+describe("testing symbol component O", () => {
+  it("should render without any problems", () => {
+    render(<SymbolO />);
+
+    let circleDom = screen.getByTestId("circle");
+    expect(circleDom).toBeTruthy();
+    expect(circleDom).toHaveStyle("width: 45px;");
+    expect(circleDom).toHaveStyle("border-radius: 25px;");
+    expect(circleDom).toHaveStyle("background-color: transparent;");
+  });
+});
